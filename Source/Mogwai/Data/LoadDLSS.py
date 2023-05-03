66d9ed2c091470abd00cca5306be2420706f3e60
Songyin Wu <57281631+Poiw@users.noreply.github.com>
2023-05-02 18:04:52 -0700

    add test dlss

--- Source/Mogwai/Data/LoadDLSS.py
+++ Source/Mogwai/Data/LoadDLSS.py
@@ -4,14 +4,23 @@ def render_graph_TestDLSS():
     g = RenderGraph("TestDLSS")
 
     loadRenderPassLibrary("PreprocessDLSS.dll")
+    loadRenderPassLibrary("DLSSPass.dll")
 
     PreprocessDLSS = createPass("PreprocessDLSS")
     g.addPass(PreprocessDLSS, "PreprocessDLSS")
+
+    DLSS = createPass("DLSSPass", {'enabled': True, 'profile': DLSSProfile.Balanced, 'motionVectorScale': DLSSMotionVectorScale.Relative, 'isHDR': True, 'sharpness': 0.0, 'exposure': 0.0})
+    g.addPass(DLSS, "DLSS")
+
+    g.addEdge("PreprocessDLSS.mvec",            "DLSS.mvec")
+    g.addEdge("PreprocessDLSS.depth",           "DLSS.depth")
+    g.addEdge("PreprocessDLSS.color",           "DLSS.color")
 
     # Outputs
     g.markOutput("PreprocessDLSS.depth")
     g.markOutput("PreprocessDLSS.color")
     g.markOutput("PreprocessDLSS.mvec")
+    g.markOutput("DLSS.output")
 
     return g
 
